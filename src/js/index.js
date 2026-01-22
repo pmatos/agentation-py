7ffa2735988dd08e6f8685d84df9c709c0d15115
Paulo Matos <p@ocmatos.com>
2026-01-22 17:34:17 +0100

    chore: set up JavaScript build infrastructure with esbuild

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+/**
+ * Agentation - Visual feedback tool for AI coding agents
+ * @version 0.1.0
+ */
+
+(function() {
+  'use strict';
+
+  console.log('Agentation loaded');
+
+  // Placeholder - will be replaced with actual implementation
+  window.__AGENTATION_LOADED__ = true;
+})();
